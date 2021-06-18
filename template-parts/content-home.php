afe6638d4e21912e2e3594b591348f17c448435c
moxyinc <moxyinc@gmail.com>
2021-06-18 03:18:45 +0000

    Roll back to tailwindcss 2.1.2

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -5,8 +5,8 @@
 </header>
 
 
-<p class="text-red-900 ">
-Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde voluptatem debitis vero qui fuga ex nihil pariatur adipisci nam animi porro asperiores dolore eveniet incidunt, id mollitia molestiae similique dolorum.
+<h1 class="text-indigo-300 font-bold text-4xl underline">This Is the New Text for Spitfire-NS</h1>
+<p class="mt-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde voluptatem debitis vero qui fuga ex nihil pariatur adipisci nam animi porro asperiores dolore eveniet incidunt, id mollitia molestiae similique dolorum.
 </p>
 
 <blockquote>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</blockquote>
