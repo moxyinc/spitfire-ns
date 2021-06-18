1c2ede1f86150d11d500c988b66b7db726893092
moxyinc <moxyinc@gmail.com>
2021-06-18 17:55:44 +0000

    From ACF Docs

--- page-home.php
+++ page-home.php
@@ -2,14 +2,48 @@
 <?php get_header(); ?>
 
 
-<h1 class="text-ns-blue font-bold text-5xl">Current Listings</h1>
+<h1 class="text-ns-blue font-bold text-5xl mb-12">Current Listings</h1>
+
+
+
+<?php 
+
+// query
+$the_query = new WP_Query(array(
+	'post_type'			=> 'listing',
+	'posts_per_page'	=> -1,
+	'meta_key'			=> 'featured',
+	'orderby'			=> 'meta_value',
+	'order'				=> 'DESC'
+));
+
+?>
+<?php if( $the_query->have_posts() ): ?>
+	<ul>
+	<?php while( $the_query->have_posts() ) : $the_query->the_post(); 
+		
+		$class = get_field('featured') ? 'class="featured font-bold"' : '';
+		
+		?>
+		<li <?php echo $class; ?>>
+			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		</li>
+	<?php endwhile; ?>
+	</ul>
+<?php endif; ?>
+
+<?php wp_reset_query();	 // Restore global post data stomped by the_post(). ?>
 
 
 
 
 
 
-<div class="container mx-auto">
+
+
+
+
+<div class="container mx-auto mt-12">
 		
 	
 	<?php if (have_posts()): ?>
